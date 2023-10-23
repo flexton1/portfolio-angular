0b0386881e891856219eff8c029b8dbd85e58acd
Harun J <harunjasarevic00@gmail.com>
2023-10-24 01:14:43 +0200

    update 24.10.2023.

--- src/app/components/portfolio/cards.ts
+++ src/app/components/portfolio/cards.ts
@@ -1,28 +1,38 @@
-export const cardTops = [
+import {CardTop} from "./card-top";
+
+export const cardTops: CardTop[] = [
     {
-        img: 'assets/img/ideallista.png',
+        img: 'assets/img/ideallista2.png',
         title: 'Ideallista V2',
-        card_text: 'An app for listening music.',
-        git_backend: 'https://github.com/flexton1/IdealListaV2-spring',
-        git_frontend: 'https://github.com/flexton1/idealListaV2',
-        live_view: 'https://flexton1.github.io/idealListaV2/'
-    },
-    {
-        img: 'assets/img/weathercard.jpg',
-        title: 'My weather app',
-        card_text: 'A quick app to get information on current weather.',
+        card_text: 'An app for your best music experience.',
         git_backend: '',
-        git_frontend: 'https://github.com/flexton1/weather-app',
-        live_view: 'https://flexton1.github.io/weather-app/'
+        git_frontend: '',
+        live_view: 'https://ideallista.net/'
     },
     {
-        img: 'assets/img/studentska-sluzba.png',
-        title: 'Studenska sluzba (angular)',
-        card_text: 'An app for saving student data in a school or gymnasium.',
-        git_backend: 'https://github.com/flexton1/studentska-sluzba',
-        git_frontend: 'https://github.com/flexton1/take-home-page-angular',
-        live_view: 'https://flexton1.github.io/take-home-page-angular/'
+      img: 'assets/img/vodovodkotor.png',
+      title: 'Vodovod Kotor',
+      card_text: 'Water reading management system.',
+      git_backend: '',
+      git_frontend: '',
+      live_view: 'https://app.vodovodkotor.net/'
     },
+    // {
+    //     img: 'assets/img/weathercard.jpg',
+    //     title: 'My weather app',
+    //     card_text: 'A quick app to get information on current weather.',
+    //     git_backend: '',
+    //     git_frontend: 'https://github.com/flexton1/weather-app',
+    //     live_view: 'https://flexton1.github.io/weather-app/'
+    // },
+    // {
+    //     img: 'assets/img/studentska-sluzba.png',
+    //     title: 'Studenska sluzba (angular)',
+    //     card_text: 'An app for saving student data in a school or gymnasium.',
+    //     git_backend: 'https://github.com/flexton1/studentska-sluzba',
+    //     git_frontend: 'https://github.com/flexton1/take-home-page-angular',
+    //     live_view: 'https://flexton1.github.io/take-home-page-angular/'
+    // },
     {
         img: 'assets/img/portfolio.jpg',
         title: 'My portfolio site (current site)',
@@ -31,12 +41,12 @@ export const cardTops = [
         git_frontend: 'https://github.com/flexton1/studentska-sluzba',
         live_view: 'https://flexton1.github.io/portfolio-angular/'
     },
-    {
-        img: 'assets/img/studentska-sluzba-react.png',
-        title: 'Studentska sluzba (React)',
-        card_text: 'An app for saving student data in a school or gymnasium built with React and NodeJS.',
-        git_backend: 'https://github.com/flexton1/studentska-sluzba-node',
-        git_frontend: 'https://github.com/flexton1/studentska-sluzba-react',
-        live_view: ''
-    }
+    // {
+    //     img: 'assets/img/studentska-sluzba-react.png',
+    //     title: 'Studentska sluzba (React)',
+    //     card_text: 'An app for saving student data in a school or gymnasium built with React and NodeJS.',
+    //     git_backend: 'https://github.com/flexton1/studentska-sluzba-node',
+    //     git_frontend: 'https://github.com/flexton1/studentska-sluzba-react',
+    //     live_view: ''
+    // }
 ]
